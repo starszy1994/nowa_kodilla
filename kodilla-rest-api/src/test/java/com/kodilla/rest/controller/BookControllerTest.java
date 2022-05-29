68a58864969202114eea256ac3f26a54eb1152d1
Mateusz Filip <starszy1994@gmail.com>
2022-05-29 22:00:30 +0200

    api testy2

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
@@ -13,19 +13,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BookControllerTest {
 
     @Test
-    void shouldFetchBooks() {
+    public void shouldFetchBooks() {
         //given
         BookService bookServiceMock = Mockito.mock(BookService.class);
         BookController bookController = new BookController(bookServiceMock);
         List<BookDto> booksList = new ArrayList<>();
-        booksList.add(new BookDto("Title 1", "Author 1"));
-        booksList.add(new BookDto("Title 2", "Author 2"));
+        booksList.add(new BookDto("title 1", "author 1"));
+        booksList.add(new BookDto("title 2", "author 2"));
         Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
-
         //when
         List<BookDto> result = bookController.getBooks();
-
         //then
         assertThat(result).hasSize(2);
+    }
+    @Test
+    public void shouldAddBooks() {
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+
+        BookDto bookDto = new BookDto("title 3", "author 3");
+        bookController.addBook(bookDto);
+        Mockito.verify(bookServiceMock).addBook(bookDto);
     }
 }
