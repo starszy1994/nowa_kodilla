5d4c85fcddea88dddad9278b36ab6b7a9b8d69f4
Mateusz Filip <starszy1994@gmail.com>
2022-05-31 22:05:25 +0200

    allegro poprawka

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,6 +1,5 @@
 package com.kodilla.selenium.allegro;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,32 +8,23 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class AllegroTestingApp {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "c:/selenium-drivers/Chrome/chromedriver.exe");
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:/selenium-drivers/chrome/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
 
         WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//html/body/div[2]/div[1]/div/div[2]/div[2]/button[1]")));
+        WebElement inputField = driver.findElement(By.xpath("//html/body/div[2]/div[1]/div/div[2]/div[2]/button[1]"));
+        inputField.click();
 
-        //podana w teorii opcja z Alertem z jakiegoś powodu nie działa, więc klikam przycisk w popup po ścieżce absolutnej
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dialog-content")));
-        WebElement gdprPopup = driver.findElement(By.cssSelector("button[data-role='accept-consent']"));
-        gdprPopup.click();
+        WebElement category = driver.findElement(By.xpath("//*[@aria-label=\"Kategoria i opcje wyszukiwania\"]"));
+        Select categorySelect = new Select(category);
+        categorySelect.selectByIndex(3);
 
-        WebElement searchField = driver.findElement(By.cssSelector("div[data-box-name='allegro.metrumHeader.search'] > div > form > input"));
-        searchField.sendKeys("Mavic mini");
-
-        WebElement categoryCombo = driver.findElement(By.cssSelector("div[data-role='filters'] > div > select"));
-        Select categorySelect = new Select(categoryCombo);
-        categorySelect.selectByValue("/kategoria/elektronika");
-        searchField.submit();
-
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div[data-box-name='items-v3'] > div >div > section > article")));
-        List<WebElement> searchResults = driver.findElements(By.cssSelector("div[data-box-name='items-v3'] > div >div > section > article"));
-        System.out.println(searchResults.get(15).getText());
-
+        WebElement drone = driver.findElement(By.xpath("//*[@placeholder=\"czego szukasz?\"]"));
+        drone.sendKeys("Mavic mini");
+        drone.submit();
     }
 }
